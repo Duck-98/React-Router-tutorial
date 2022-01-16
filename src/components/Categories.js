951deeb54ea00d73e8e180c6c9529a0302ce28f2
duck <godqhr2256@gmail.com>
2022-01-16 22:12:05 +0900

    카테고리 스타일적용

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const categories = [
   {
@@ -57,13 +57,34 @@ const Category = styled.div`
   & + & {
     margin-left: 1rem;
   }
+
+  ${(props) =>
+    props.active &&
+    css`
+      font-weight: 600;
+      border-bottom: 2px solid #22b8cf;
+      color: #22b8cf;
+      &:hover {
+        color: #3bc9db;
+      }
+    `}
+
+  %+ & {
+    margin-left: 1rem;
+  }
 `;
 
-const Categories = () => {
+const Categories = ({ onSelect, category }) => {
   return (
     <CategoriesBlock>
       {categories.map((c) => (
-        <Category key={c.name}>{c.text}</Category>
+        <Category
+          key={c.name}
+          active={category === c.name}
+          onClick={() => onSelect(c.name)}
+        >
+          {c.text}
+        </Category>
       ))}
     </CategoriesBlock>
   );
