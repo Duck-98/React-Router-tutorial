f3ba7358deea4aad2f687d7fcc565b922aa8317e
duck <godqhr2256@gmail.com>
2022-01-16 20:39:56 +0900

    API 요청

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -9,31 +10,46 @@ const NewsListBlock = styled.div`
   margin: 0 auto;
   margin-top: 2rem;
   @media screen and (max-width: 768px) {
-    width: 100%;
+    width: 80%;
     padding-left: 1rem;
     padding-right: 1rem;
   }
 `;
 
-const sampleArticle = {
-  title: '제목',
-  description: '내용',
-  url: 'https://google.com',
-  urlToImage: 'https://via.placeholder.com/160',
-};
-
 const NewsList = () => {
+  const [loading, setLoading] = useState(false);
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    // api 받아오기
+    const apiData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=c0c66f0cafff4e69889ab277f6829b32',
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    apiData();
+  }, []);
+  // 대기
+  if (loading) {
+    return <NewsListBlock>잠시만 대기 부탁드립니다.</NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
+
   return (
     <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
     </NewsListBlock>
   );
 };
-
 export default NewsList;
